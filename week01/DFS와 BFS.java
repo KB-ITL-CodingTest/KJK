2854857e8e4869bf997298171f1a4e3b5d4faef3
KJK1023 <102352312+KJK1023@users.noreply.github.com>
2025-04-02 23:38:58 +0900

    DFS와 BFS.java

--- week01/DFS와 BFS.java
+++ week01/DFS와 BFS.java
@@ -1,1 +1,69 @@
+import java.util.*;
 
+public class Main {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();  
+        int M = scanner.nextInt(); 
+        int V = scanner.nextInt();  
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+
+        visited = new boolean[N + 1];
+        dfs(V);
+        System.out.println();
+
+
+        visited = new boolean[N + 1];
+        bfs(V);
+        System.out.println();
+    }
+
+
+    static void dfs(int node) {
+        visited[node] = true;
+        System.out.print(node + " ");
+
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int next : graph[node]) {
+                if (!visited[next]) {
+                    queue.add(next);
+                    visited[next] = true;
+                }
+            }
+        }
+    }
+}
